3cd01cc10dc66f9ac51babce5b3d3b7969425505
Androbuddy <anirudhchimpidi8@gmail.com>
2022-05-06 00:15:29 +0530

    New navigation mode for mobile view
    Signed-off-by: Androbuddy <anirudhchimpidi8@gmail.com>

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,19 +9,20 @@
 			<li><a class="hover:underline transition ease-in-out duration-300 cursor-pointer" href="/blogs/">Blogs</a></li>
 			<li><a class="hover:underline transition ease-in-out duration-300 cursor-pointer" href="/comics/">Comic Strips</a></li>
 		</ul>
-		<div class="block lg:hidden" @click="overflow = true">
-			<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" v-if="overflow">
-				<path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" fill="black"/>
-			</svg>
-			<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" v-else>
-				<path d="M3 6H21V8H3V6ZM3 11H21V13H3V11ZM3 16H21V18H3V16Z" fill="black"/>
-			</svg>
-		</div>
 	</header>
+	<div class="fixed z-50 bottom-4 right-4 flex items-center justify-center gap-4 lg:hidden bg-white p-4  rounded-lg shadow-md" @click="overflow = true">
+		<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" v-if="overflow">
+			<path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" fill="black"/>
+		</svg>
+		<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" v-else>
+			<path d="M3 6H21V8H3V6ZM3 11H21V13H3V11ZM3 16H21V18H3V16Z" fill="black"/>
+		</svg>
+		Menu
+	</div>
 	<div class="fixed z-50 inset-0 lg:hidden" v-if="overflow">
 		<div class="fixed inset-0 bg-black/20 backdrop-blur-sm" @click="overflow = false"></div>
-		<div class="fixed top-4 right-4 w-full max-w-xs bg-white rounded-lg shadow-lg p-6 text-base font-semibold">
-			<button @click="overflow = false" class="absolute top-5 right-5 w-8 h-8 flex items-center justify-center text-slate-500 hover:text-slate-600">
+		<div class="fixed bottom-4 right-4 w-full max-w-xs bg-white rounded-lg shadow-lg p-6 text-base font-semibold">
+			<button @click="overflow = false" class="absolute bottom-5 right-5 w-8 h-8 flex items-center justify-center text-slate-500 hover:text-slate-600">
 				<svg viewBox="0 0 10 10" class="w-2.5 h-2.5 overflow-visible" aria-hidden="true">
 					<path d="M0 0L10 10M10 0L0 10" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"></path>
 				</svg>
