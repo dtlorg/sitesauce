19e17a2d9a18603c91dd40d493ad23bcbf6c2e5e
Androbuddy <anirudhchimpidi8@gmail.com>
2022-05-20 18:37:26 +0530

    Update gallery layout
    Signed-off-by: Androbuddy <anirudhchimpidi8@gmail.com>

--- src/Pages/App.vue
+++ src/Pages/App.vue
@@ -16,8 +16,14 @@ import ProjectPage from '../components/ProjectPage.vue';
 
 		<div class="flex flex-col gap-12 mx-6 lg:mx-60 my-8">
 			<carousel :items-to-show="1" :autoplay="3000" :transition="600" :wrap-around="true" class="bg-black/5 rounded-xl">
-				<slide v-for="slide in 10" :key="slide">
-					<img src="../assets/logo.png" class="p-12">
+				<slide v-for="slide in prs" :key="slide">
+					<div class="flex justify-center items-end h-64 rounded-xl hover:scale-90 transition-transform cursor-pointer"
+						@click="openURL(slide.website)">
+						<div class="flex flex-col gap-2">
+								<span class="ml-6 w-52 overflow-hidden font-bold">{{ slide.project }}</span>
+								<span class="ml-6 mb-6 w-52 overflow-hidden">{{ slide.team }}</span>
+							</div>
+					</div>
 				</slide>
 
 				<template #addons>
@@ -40,16 +46,63 @@ import ProjectPage from '../components/ProjectPage.vue';
 						v-for="i in prs"
 						:key="i"
 						class="flex justify-start items-end bg-black/5 h-64 rounded-xl hover:scale-90 transition-transform cursor-pointer"
-						@click="project = true,
+						@click="openURL(i.website)"
+							@mouseenter="proj = true,
 							pproject = i.project,
-							pteam = i.team,
-							plink = i.website,
-							pmentor = i.mentor,
-							pdesc = i.desc">
-						<span class="ml-6 mb-6 w-52 overflow-hidden">{{ i.team }}</span>
+								pteam = i.team,
+								plink = i.website,
+								pmentor = i.mentor,
+								pdesc = i.desc">
+						<div class="flex flex-col gap-2">
+								<span class="ml-6 w-52 overflow-hidden font-bold">{{ i.project }}</span>
+								<span class="ml-6 mb-6 w-52 overflow-hidden">{{ i.team }}</span>
+							</div>
 					</div>
 				</div>
 			</div>
+
+			<transition name="fade">
+			<div class="fixed right-0 top-0 z-50 flex h-screen w-screen lg:w-96 overflow-y-scroll rounded-xl" v-if="proj" style="background-color: #F2F2F2;" :class="{'fade-enter-active': proj}">
+				<div class="flex flex-col mx-6 my-6 min-h-screen">
+				<!-- Team info -->
+				<span class="font-bold flex gap-4" @click="proj = !proj">
+					<svg
+						width="24"
+						height="24"
+						viewBox="0 0 24 24" fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+						class="hover:scale-90 transition ease-in-out duration-300 cursor-pointer">
+						<path d="M21 11H6.83L10.41 7.41L9 6L3 12L9 18L10.41 16.58L6.83 13H21V11Z" fill="black"/>
+					</svg>
+				</span>
+				<div class="flex my-8">
+					<div class="flex flex-col gap-4 w-full">
+						<div class="bg-black/10 w-full h-32 rounded-xl"></div>
+						<div>
+							<h1 class="text-base w-64 lg:w-auto overflow-hidden"><span class="font-bold">{{ pproject }}</span><br>{{ pteam }}</h1>
+							<h2 class="text-s">Mentor: {{ pmentor }}</h2>
+							<h1 @click="openURL(plink)" class="text-blue-500 hover:underline cursor-pointer transition ease-in-out duration-300 w-64 lg:w-auto overflow-hidden">{{ pteam }} website</h1>
+						</div>
+					</div>
+				</div>
+
+				<!-- Desc-->
+				<div class="my-8">
+					<h1 class="font-bold">Description:</h1>
+					<h1 class="mt-6">{{ pdesc }}</h1>
+				</div>
+
+				<!-- Comments -->
+				<div>
+					<h1 class="font-bold">Post a comment</h1>
+					<div class="h-52 my-6">
+						<textarea class="bg-black/5 rounded-xl p-4 w-full focus:outline-none" name="" rows="5" />
+						<button class="mt-4 bg-slate-900 hover:bg-slate-700 focus:outline-none text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto">Submit</button>
+					</div>
+				</div>
+			</div>
+			</div>
+			</transition>
 
 			<div class="flex flex-col gap-6">
 				<div class="flex justify-between">
@@ -104,11 +157,17 @@ export default {
 		plink: '',
 		pmentor: '',
 		pdesc: '',
+		proj: false
 	}),
 	components: {
 		Carousel,
 		Slide,
 		Navigation,
+	},
+	methods: {
+		openURL(url) {
+			window.open(url);
+		}
 	},
 	mounted() {
 		axios
@@ -127,6 +186,11 @@ export default {
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
 
+::-webkit-scrollbar {
+	width: 0px;
+	background: transparent;
+}
+
 #app {
 	font-family: 'Montserrat', sans-serif;
 	-webkit-font-smoothing: antialiased;
@@ -141,5 +205,12 @@ export default {
 
 .carousel__pagination-item {
 	color: black;
+}
+
+.fade-enter-active, .fade-leave-active {
+	transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+	opacity: 0;
 }
 </style>
